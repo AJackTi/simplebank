b68eed81dc38c452038a1be10e9fafb967bc974a
AJackTi <dtrong97vn@gmail.com>
2023-03-30 16:50:03 +0700

    feat: run migration db inside golang main function

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 	"github.com/AJackTi/simplebank/gapi"
 	"github.com/AJackTi/simplebank/pb"
 	"github.com/AJackTi/simplebank/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -32,10 +35,26 @@ func main() {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
+	// run db migration
+runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	runGinServer(&config, store)
 	// go runGatewayServer(&config, store)
 	// runGrpcServer(&config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance:", err)
+	}
+
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGrpcServer(config *util.Config, store db.Store) {
